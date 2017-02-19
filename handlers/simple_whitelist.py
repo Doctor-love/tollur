b239905ff09591d21d988b4d322f8dd633790d2e
Joel Rangsmo <joel@rangsmo.se>
2017-02-19 12:59:06 +0000

    Added support for manipulating messages

--- handlers/simple_whitelist.py
+++ handlers/simple_whitelist.py
@@ -3,36 +3,37 @@
 import logging
 _log = logging.getLogger('tollur')
 
-class Verifier:
-    '''Verifier class for a simple white list plugin''' 
+class Handler:
+    '''Handler class for a simple white list plugin''' 
 
     def __init__(self, conf):
         self.whitelist_domains = conf['whitelist_domains'].split(',')
         self.include_subdomains = conf.getboolean('include_subdomains')
 
-    def verify(self, msg_id, peer, sender, recipients, data):
+    def process(self, msg):
         '''Function that is used to determine the status of received mail'''
         
         _log.debug('Checking if recipients domains are in whitelist')
 
-        for recipient in recipients:
+        for recipient in msg.recipients:
             recipient_parts = recipient.split('@')
 
             if len(recipient_parts) != 2:
                 _log.info(
                     'Something fishy is going on in message "%s" - '
                     'peer "%s" is trying to send a message to "%s"!'
-                    % (msg_id, str(peer), str(recipient_parts)))
+                    % (msg.mid, str(msg.peer), str(recipient_parts)))
 
-                return False
+                return False, msg
 
             domain = recipient_parts[1]
+            _log.debug(domain)
 
             if domain not in self.whitelist_domains:
                 _log.info('Domain "%s" is not whitelisted!' % domain)
 
-                return False
+                return False, msg
 
-            _log.info('Everything looks good - accepting "%s"' % msg_id)
+        _log.info('Everything looks good - accepting "%s"' % msg.mid)
 
-            return True
+        return True, msg
